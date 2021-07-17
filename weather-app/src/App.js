6a209a3e789210b893c6b162ec2d34021081a358
Sunil Singh <ssuniljust4u@gmail.com>
2021-07-17 15:28:48 +0530

    Third commit

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -4,6 +4,11 @@ function App() {
   return (
     <div className="App">
       <h1>Welcom to Weather Forcast</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore
+        repellat beatae veritatis saepe! Nam labore consequatur, enim molestiae
+        quo necessitatibus repellat dolores fugiat saepe.
+      </p>
     </div>
   );
 }
